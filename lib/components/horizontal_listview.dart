5556baca5b5885d7e9edb492ddeb3834b22bad3b
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-08-06 18:24:21 +0530

    category widget

--- lib/components/horizontal_listview.dart
+++ lib/components/horizontal_listview.dart
@@ -14,8 +14,53 @@ class HorizontalList extends StatelessWidget {
           Icon(Icons.help),
           Icon(Icons.help),
           Icon(Icons.help),
+          Icon(Icons.help),
+          Icon(Icons.help),
+          Icon(Icons.help),
+          Icon(Icons.help),
+          Icon(Icons.help),
+          Icon(Icons.help),
+          Icon(Icons.help),
+          Icon(Icons.help),
+          Icon(Icons.help),
+          Icon(Icons.help),
+          Icon(Icons.help),
+          Icon(Icons.help),
+          Icon(Icons.help),
+          Icon(Icons.help),
+          Icon(Icons.help),
+          Icon(Icons.help),
+          Icon(Icons.help),
+          Icon(Icons.help),
         ],
       ),
+    );
+  }
+}
+
+class Category extends StatelessWidget {
+  final String image_location;
+  final String image_caption;
+
+  Category({this.image_location, this.image_caption});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(2.0),
+      child: InkWell(
+        onTap: (){
+
+        },
+        child: ListTile(
+          title: Image.asset(image_location),
+          subtitle: Text(
+            image_caption,
+          ),
+        ),
+      ),
+
     );
   }
 }
