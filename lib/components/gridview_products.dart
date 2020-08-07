2e68c67107f8d6994c527fa0b6b44c1165d29ac3
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-08-07 23:42:28 +0530

    Add list tile

--- lib/components/gridview_products.dart
+++ lib/components/gridview_products.dart
@@ -12,12 +12,73 @@ class _ProductsState extends State<Products> {
       "picture":"assets/images/products/blazer1.jpeg",
       "old_price":120,
       "price":85,
-    }
+    },
+
+    {
+      "name":"Blazer",
+      "picture":"assets/images/products/dress1.jpeg",
+      "old_price":120,
+      "price":85,
+    },
+
   ];
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return GridView.builder(
+      itemCount: product_list.length,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+      itemBuilder: (BuildContext context, int index){
+        return SingleProducts(
+          prod_name: product_list[index]['name'],
+          prod_picture: product_list[index]['picture'],
+          prod_old_price: product_list[index]['old_price'],
+          prod_price: product_list[index]['price'],
+        );
+      },
+    );
+  }
+}
+
+class SingleProducts extends StatelessWidget {
+  final prod_name;
+  final prod_picture;
+  final prod_old_price;
+  final prod_price;
+
+  const SingleProducts({this.prod_name, this.prod_picture, this.prod_old_price, this.prod_price});
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Hero(
+        tag: prod_name,
+        child: Material(
+          child: InkWell(
+            onTap: (){
+
+            },
+            child: GridTile(
+              footer: Container(
+                color: Colors.white,
+                child: ListTile(
+                  leading: Text(
+                    prod_name,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black
+                    ),
+                  ),
+                ),
+              ),
+              child: Image.asset(
+                prod_picture,
+                fit: BoxFit.cover ,
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
