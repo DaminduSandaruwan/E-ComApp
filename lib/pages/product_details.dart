a6931e58a5a7b01baf2e9471098c6bba43399685
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-08-09 21:03:46 +0530

    create app bar

--- lib/pages/product_details.dart
+++ lib/pages/product_details.dart
@@ -8,7 +8,28 @@ class ProductDetails extends StatefulWidget {
 class _ProductDetailsState extends State<ProductDetails> {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.1,
+        backgroundColor: Colors.black,
+        title: Text("ShopApp"),
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            color: Colors.white,
+            onPressed: (){
+
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: (){
+
+            },
+          ),
+        ],
+      ),
       
     );
   }
